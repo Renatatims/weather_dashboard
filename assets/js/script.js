143fc90494f27b7ebc9acea85743105f1f5c4f94
ReTims <renatatims@gmail.com>
2022-09-01 16:41:27 -0500

    Weather variables added to current weather

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,22 @@
-var APIkey = "&appid=4b240ff285db6735e81b43a05878c1bf"
-var searchBtn = $("#search")
-var weatherUrl = "https://api.openweathermap.org/data/2.5/weather"
+var APIkey = "&appid=4b240ff285db6735e81b43a05878c1bf";
+var searchBtn = $("#search");
+var weatherUrl = "https://api.openweathermap.org/data/2.5/weather";
+
+// Weather Forecast - Current and 5-days
+var weatherForecastEl = $("#weatherForecast");
+
+// Current Weather Variables
+var chosenCityEl = $("#chosenCity");
+var temperatureEl = $("#temperature");
+var windEl = $("#wind");
+var humidityEl = $("#humidity");
+var uvIndexEl = $("#uvIndex");
 
 // Function Init - calls Search City function - once the user types in the city and click the Search button, then fetch will be executed.
 
 function init(){
-	searchCity ();
+	weatherForecastEl.hide();
+	searchCity();
 }
 
 // Function - Event Listener to Search Button
@@ -31,8 +42,24 @@ function weatherCity(userInputCity){
 		if(cityResponse.ok){
 			cityResponse.json().then(function (response){
 
+				weatherForecastEl.show();
+
 				var results = response;
+				var cityName = results.name;
+				var cityTemperature = results.main.temp;
+				var cityWind = results.wind.speed;
+				var cityHumidity = results.main.humidity;
+
+				chosenCityEl.text(cityName);
+				temperatureEl.text("Temperature: " + cityTemperature);
+				windEl.text("Wind: " + cityWind);
+				humidityEl.text("Humidity: " + cityHumidity);
+
+
 				console.log(results);
+
+
+
 			})
 		}
 	})
